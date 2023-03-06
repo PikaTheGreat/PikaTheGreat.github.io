455d3eabf9d6e3084414ddf305b6c0b83ef5da4c
PikaTheGreat <99338537+PikaTheGreat@users.noreply.github.com>
2023-03-06 23:45:31 +0530

    Hh

---
+++ index.js
@@ -0,0 +1,32 @@
+html
+<!DOCTYPE html>
+<html>
+<head>
+    <title>This site is under development</title>
+    <style>
+        body {
+            background-color: #3b5998; /* a cool blue color */
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; /* a fancy font */
+            color: white;
+            text-align: center;
+            padding-top: 50px;
+        }
+
+        h1 {
+            font-size: 4em;
+            margin-bottom: 50px;
+        }
+
+        p {
+            font-size: 2em;
+            line-height: 1.5;
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+<body>
+    <h1>This site is under development</h1>
+    <p>Please check back soon for updates.</p>
+</body>
+</html>
+
